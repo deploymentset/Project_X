d3fefdf5441709858b65be7e311a6fe844550d8c
deploymentset <109267375+deploymentset@users.noreply.github.com>
2023-06-18 21:04:00 +0530

    Update version.tf

--- infrastructure/aws_vpc_module/version.tf
+++ infrastructure/aws_vpc_module/version.tf
@@ -1,15 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.67.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "4.67.0"
+#     }
+#   }
+# }
 
-provider "aws" {
-    shared_config_files      = ["C:/Users/Rajat/.aws/config"]
-    shared_credentials_files = ["C:/Users/Rajat/.aws/credentials"]
-    profile                  = "project_x"
-}
+# provider "aws" {
+#     shared_config_files      = ["C:/Users/Rajat/.aws/config"]
+#     shared_credentials_files = ["C:/Users/Rajat/.aws/credentials"]
+#     profile                  = "project_x"
+# }
 
